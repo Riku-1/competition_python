b5aa5641938e6a67439acd623092f725885023d8
Riku-1 <osaka.riku@gmail.com>
2021-01-21 11:59:47 +0900

    fix

--- sort/selection_sort.py
+++ sort/selection_sort.py
@@ -1,6 +1,7 @@
-def find_min_index(li: []):
-    min_index = 0
-    for i in range(len(li)):
+def find_min_index(li: [], start_index: int):
+    min_index = start_index
+    for i in range(start_index, len(li)):
+        print(i)
         if li[i] < li[min_index]:
             min_index = i
     return min_index
@@ -14,5 +15,5 @@ def switch(li: [], a: int, b: int):
 
 def selection_sort(li: []):
     for i in range(len(li)):
-        min_index = find_min_index(li[i:])
-        switch(li, i, i + min_index)
+        min_index = find_min_index(li, i)
+        switch(li, i, min_index)
